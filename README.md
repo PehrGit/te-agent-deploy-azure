d1d975755b857e465fce71411dcb5b546d723bfd
Bostjan Skufca <bostjan@a2o.si>
2016-09-03 23:16:03 +0000

    Add README, remove fixed url from doc.php

---
+++ README.md
@@ -0,0 +1,7 @@
+# ThousandEyes Enterprise Agent - Deploy to Microsoft Azure
+
+<a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fbostjan%2Fte-agent-deploy-azure%2Fmaster%2Fazuredeploy.json" target="_blank">
+    <img src="http://azuredeploy.net/deploybutton.png"/>
+</a>
+
+Deploy ThousandEyes Enterprise agent into Microsoft Azure in seconds.
--- doc.php
+++ doc.php
@@ -1,11 +1,32 @@
 <?php
 
-$urlRaw = "https://www.a2o.si/tmp/te/ea-azure/azuredeploy.json";
+/*
+ * Development helper document
+ */
+
+function url_origin( $s, $use_forwarded_host = false )
+{
+    $ssl      = ( ! empty( $s['HTTPS'] ) && $s['HTTPS'] == 'on' );
+    $sp       = strtolower( $s['SERVER_PROTOCOL'] );
+    $protocol = substr( $sp, 0, strpos( $sp, '/' ) ) . ( ( $ssl ) ? 's' : '' );
+    $port     = $s['SERVER_PORT'];
+    $port     = ( ( ! $ssl && $port=='80' ) || ( $ssl && $port=='443' ) ) ? '' : ':'.$port;
+    $host     = ( $use_forwarded_host && isset( $s['HTTP_X_FORWARDED_HOST'] ) ) ? $s['HTTP_X_FORWARDED_HOST'] : ( isset( $s['HTTP_HOST'] ) ? $s['HTTP_HOST'] : null );
+    $host     = isset( $host ) ? $host : $s['SERVER_NAME'] . $port;
+    return $protocol . '://' . $host;
+}
+
+function full_url( $s, $use_forwarded_host = false )
+{
+    return url_origin( $s, $use_forwarded_host ) . $s['REQUEST_URI'];
+}
+
+$selfUrl = full_url($_SERVER);
+$urlRaw   = preg_replace('#/doc.php$#', '/azuredeploy.json', $selfUrl);
 $urlEnc = urlencode($urlRaw);
 
 ?>
 <html>
-
     <body>
         <a href="https://portal.azure.com/#create/Microsoft.Template/uri/<?= $urlEnc ?>" target="_blank">Deploy!</a>
     </body>

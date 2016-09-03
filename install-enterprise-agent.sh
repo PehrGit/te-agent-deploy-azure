fae48dfa7ef9237cc76eb70c31f5d876141f8274
Bostjan Skufca <bostjan@a2o.si>
2016-09-03 22:32:11 +0000

    Fix tput bug in install agent script

--- install-enterprise-agent.sh
+++ install-enterprise-agent.sh
@@ -2,6 +2,17 @@
 
 
 
+### Workaround
+#
+# The install_thousandeyes.sh uses 'tput' command which expects to be run on console.
+# If no TERM variable is set, this error message appears and messes up exit status, which
+# is reported as deployment failure in Azure:
+#   "tput: No value for $TERM and no -T specified"
+#
+export TERM=xterm
+
+
+
 ### Args
 #
 ACCOUNT_GROUP_TOKEN="$1"
@@ -25,4 +36,4 @@ apt-get -y upgrade
 #
 curl -Os https://downloads.thousandeyes.com/agent/install_thousandeyes.sh
 chmod +x install_thousandeyes.sh
-./install_thousandeyes.sh   -f   $ENABLE_BROWSER_BOT_FLAG   $ACCOUNT_GROUP_TOKEN
+./install_thousandeyes.sh   -f   $ENABLE_BROWSER_BOT_FLAG   $ACCOUNT_GROUP_TOKEN > /tmp/install_thousandeyes_overall.log 2>&1
